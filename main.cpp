a48900c22580c74cc3a9a9d2388aacd4270c0d65
SHsus1122 <tjgurtn112@gmail.com>
2023-10-27 09:42:05 +0900

    1-52 중복없는 난수생성

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <stdlib.h>
+#include <ctime>
 
 using namespace std;
 
 int main() {
+	srand(time(NULL));
 
+	int list[52];
+	for (int i = 0; i < 52; i++)
+	{
+		list[i] = i + 1;
+	}
+
+	for (int i = 0; i < 4; i++)	
+	{
+		int a = rand() % 52 + 0;
+		if (list[a] != 0)
+		{
+			cout << list[a] << endl;
+			list[a] = 0;
+		}
+		else
+		{
+			continue;
+		}
+	}
+
+	return 0;
 }
